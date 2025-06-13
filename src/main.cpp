5e58ca603915ff94ec499f12e6cfb378ba97c997
Marek Godlowicz <seagumcio75@gmail.com>
2025-06-13 19:31:44 +0200

    add project files

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <arduino.h>
+
+const byte ROWS = 4;
+const byte COLLUM = 4;
+
+char keypadz[ROWS][COLLUM] {
+    {'1','2','3','A'},
+    {'4','5','6','B'},
+    {'7','8','9','C'},
+    {'*','0','#','D'}
+};
+
+void setup() {
+
+}
+
+void loop() {
+
+}
