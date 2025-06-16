f10e3b0b91077f4fe23d3f520403fd8dfa198f20
Marek Godlowicz <seagumcio75@gmail.com>
2025-06-16 22:25:28 +0200

    add prototype of keypad

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,66 @@
 #include <arduino.h>
 
 const byte ROWS = 4;
-const byte COLLUM = 4;
+const byte COLLUMS = 4;
 
-char keypadz[ROWS][COLLUM] {
-    {'1','2','3','A'},
-    {'4','5','6','B'},
-    {'7','8','9','C'},
-    {'*','0','#','D'}
-};
+byte rowPins[ROWS] = {6, 7, 8, 9};
+byte collumPins[COLLUMS] = {2, 3, 4, 5};
 
-void setup() {
+char keypad[ROWS][COLLUMS]{
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}};
 
+// char euqualCharWithKeypad(char c)
+// {
+// }
+
+// void readKeypad(char userChoice)
+// {
+// }
+
+void setup()
+{
+    Serial.begin(9600);
+
+    for (int r = 0; r < ROWS; r++)
+    {
+        // pinMode(rowPins[r], OUTPUT);
+        // digitalWrite(rowPins[r], LOW);
+        pinMode(rowPins[r], INPUT);
+    }
+
+    for (int c = 0; c < COLLUMS; c++)
+    {
+        pinMode(collumPins[c], INPUT_PULLUP);
+    }
 }
 
-void loop() {
+void loop()
+{
+    for (int r = 0; r < ROWS; r++)
+    {   
+        pinMode(rowPins[r], OUTPUT);
+        digitalWrite(rowPins[r], LOW);
 
+        Serial.println("checking " + String(r));
+
+        delayMicroseconds(20);
+
+        for (int c = 0; c < COLLUMS; c++)
+        {
+            int collumSignal = digitalRead(collumPins[c]);
+
+            if (collumSignal == LOW)
+            {
+                Serial.println("find sygnal in collum");
+                Serial.println("(" + String(r) + " , " + String(c) + ")");
+                while (digitalRead(collumPins[c]) == LOW); // czekaj aż puścisz przycisk
+                delay(100);
+                break;
+            }
+        }
+        pinMode(rowPins[r], INPUT);
+    }
 }
