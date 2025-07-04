6f25062cb68970a80ebf3723de166d09cbbaf85f
Marek Godlowicz <seagumcio75@gmail.com>
2025-07-04 20:49:38 +0200

    finish display logic

--- src/main.cpp
+++ src/main.cpp
@@ -48,6 +48,10 @@ void handleClearButton();
 
 void handleCursorModeButton();
 
+void handleForwardButton();
+
+void handleBackwardButton();
+
 void setup() {
     Serial.begin(9600);
 
@@ -77,12 +81,13 @@ unsigned long lastMillisWrite;
 bool shouldShowChar = true;
 
 int currentCharsNumber = 0;
-int maxCharsNumber = 10;
+int maxCharsNumber = 4;
 
 bool shouldImpulse = true;
 
 void loop() {
     char c = readKeypad();
+    Serial.println(currentCharsNumber);
 
     if ((millis() - lastMillis) >= 500) {
         if (isEditionMode) {
@@ -92,18 +97,28 @@ void loop() {
         }
     }
 
-    if (currentCharsNumber < maxCharsNumber) {
-        displayCharOnLCD(c);
+    if (cursorPos[0] >= pin.length()) {
+        isEditionMode = false;
+        Serial.println(pin);
+        Serial.println("chars number cursor  is: " + String(currentCharsNumber));
     }
 
-    Serial.println(pin);
+    // if (!isEditionMode) {
+    if (currentCharsNumber >= maxCharsNumber) {
+        return;
+    }
+
+    displayCharOnLCD(c);
 }
 
 void displayCharOnLCD(char c) {
     if (validChar(c)) {
         if (isdigit(c)) {
+            // if (!isEditionMode) {
+            //     currentCharsNumber++;
+            // }
             currentCharsNumber++;
-            Serial.print("currentCharsNumber inscreased");
+            Serial.println("currentCharsNumber inscreased");
             Serial.println("displaying character");
             Serial.println(String(cursorPos[0]) + " " + String(cursorPos[1]));
             lastMillisWrite = millis();
@@ -126,122 +141,159 @@ void assignKeyFucntion(char c) {
         case 'C':
             handleCursorModeButton();
             break;
+        case '*':
+            handleBackwardButton();
+            break;
+        case '#':
+            handleForwardButton();
+            break;
         default:
             break;
     }
 
     shouldImpulse = true;
     delay(20);
+}
 
-    void handleAcceptButton() {
-        lcd.setCursor(0, 1);
-        lcd.print("Accept");
-    };
+void handleAcceptButton() {
+    lcd.setCursor(0, 1);
+    lcd.print("Accept");
+};
 
-    void handleBackspaceButton() {
+void handleBackspaceButton() {
+    lcd.setCursor(cursorPos[0], cursorPos[1]);
+    cursorPos[0]--;
+    lcd.print(" ");
+    currentCharsNumber--;
+    pin.remove(pin.length() - 1);
+    Serial.println("Backspace");
+}
+
+void handleClearButton() {
+    for (int i = 0; i < maxCharsNumber; i++) {
+        lcd.setCursor(i, cursorPos[1]);
+        lcd.print(" ");
+        cursorPos[0] = 0;
+        Serial.println("clear PIN");
+    }
+    currentCharsNumber = 0;
+    pin = "";
+}
+
+void handleCursorModeButton() {
+    if (isEditionMode) {
+        currentCharsNumber = pin.length();
         lcd.setCursor(cursorPos[0], cursorPos[1]);
+        lcd.print(pin[cursorPos[0]]);
+        cursorPos[0] = pin.length();
+        isEditionMode = false;
+        // cursorPos[0]++;
+
+        Serial.println(pin[cursorPos[0]]);
+    } else {
+        lcd.setCursor(cursorPos[0], cursorPos[1]);
+        isEditionMode = true;
         cursorPos[0]--;
         lcd.print(" ");
         currentCharsNumber--;
-        pin.remove(pin.length() - 1);
-        Serial.println("Backspace");
     }
+}
 
-    void handleCursorModeButton() {
-        if (isEditionMode) {
-            lcd.setCursor(cursorPos[0], cursorPos[1]);
-            isEditionMode = false;
-            cursorPos[0]++;
-            lcd.print(pin[cursorPos[0]]);
-        } else {
-            lcd.setCursor(cursorPos[0], cursorPos[1]);
-            isEditionMode = true;
-            cursorPos[0]--;
-            lcd.print(" ");
-        }
-    }
-
-    void handleClearButton() {
-        for (int i = 0; i < maxCharsNumber; i++) {
-            lcd.setCursor(i, cursorPos[1]);
-            lcd.print(" ");
-            cursorPos[0] = 0;
-            Serial.println("clear PIN");
-        }
-        pin = "";
-    }
-
-    void writeChar(char c) {
-        Serial.println("writing char...");
+void handleForwardButton() {
+    if (cursorPos[0] != 0) {
         lcd.setCursor(cursorPos[0], cursorPos[1]);
-        lcd.print(c);
+        lcd.print(pin[cursorPos[0]]);
+    }
+    cursorPos[0]++;
+    currentCharsNumber++;
+    // lcd.setCursor(cursorPos[0], cursorPos[1]);
+}
+
+void handleBackwardButton() {
+    if (cursorPos[0] != 0) {
+        lcd.setCursor(cursorPos[0], cursorPos[1]);
+        lcd.print(pin[cursorPos[0]]);
+        cursorPos[0]--;
+        currentCharsNumber--;
+    }
+    // lcd.setCursor(cursorPos[0], cursorPos[1]);
+}
+
+void writeChar(char c) {
+    Serial.println("writing char...");
+    lcd.setCursor(cursorPos[0], cursorPos[1]);
+    lcd.print(c);
+    if (!isEditionMode) {
         pin += c;
-        delay(10);
-        cursorPos[0] += 1;
-        Serial.println("wrote char");
+    } else {
+        pin[cursorPos[0]];
     }
+    cursorPos[0] += 1;
+    // delay(10);
+    Serial.println("wrote char");
+}
 
-    void charImpulseOnDisplay(int charIndex, char c) {
-        // Serial.println("waiting to impulse...");
-        if (shouldImpulse) {
-            lastMillis = millis();
-            lcd.setCursor(cursorPos[0], cursorPos[1]);
-            displayPulsingChar(charIndex, c);
-            // Serial.print()
-        }
-        // Serial.println("impulse done");
+void charImpulseOnDisplay(int charIndex, char c) {
+    Serial.println("waiting to impulse...");
+    if (shouldImpulse) {
+        lastMillis = millis();
+        lcd.setCursor(cursorPos[0], cursorPos[1]);
+        displayPulsingChar(charIndex, c);
+        // Serial.print()
     }
+    Serial.println("impulse done");
+}
 
-    void displayPulsingChar(int charIndex, char c) {
-        if (shouldShowChar) {
-            shouldShowChar = false;
-            lcd.write(charIndex);
-            // delayMicroseconds(20);
-        } else {
-            shouldShowChar = true;
-            lcd.print(c);
-            // delayMicroseconds(20);
-        }
+void displayPulsingChar(int charIndex, char c) {
+    if (shouldShowChar) {
+        shouldShowChar = false;
+        lcd.write(charIndex);
+        // delayMicroseconds(20);
+    } else {
+        shouldShowChar = true;
+        lcd.print(c);
+        // delayMicroseconds(20);
     }
+}
 
-    char readKeypad() {
-        char userChoice = '\0';
+char readKeypad() {
+    char userChoice = '\0';
 
-        for (int r = 0; r < ROWS; r++) {
-            pinMode(rowPins[r], OUTPUT);
-            digitalWrite(rowPins[r], LOW);
+    for (int r = 0; r < ROWS; r++) {
+        pinMode(rowPins[r], OUTPUT);
+        digitalWrite(rowPins[r], LOW);
 
-            // Serial.println("checking " + String(r));
+        // Serial.println("checking " + String(r));
 
-            delayMicroseconds(20);
+        delayMicroseconds(20);
 
-            for (int c = 0; c < COLLUMS; c++) {
-                int collumSignal = digitalRead(collumPins[c]);
+        for (int c = 0; c < COLLUMS; c++) {
+            int collumSignal = digitalRead(collumPins[c]);
 
-                if (collumSignal == LOW) {
-                    Serial.println("find sygnal in collum");
-                    Serial.println("(" + String(r) + " , " + String(c) + ")");
+            if (collumSignal == LOW) {
+                Serial.println("find sygnal in collum");
+                Serial.println("(" + String(r) + " , " + String(c) + ")");
 
-                    userChoice = keypad[r][c];
+                userChoice = keypad[r][c];
 
-                    Serial.println(userChoice);
+                Serial.println(userChoice);
 
-                    while (digitalRead(collumPins[c]) == LOW);
-                    delay(100);
-                    break;
-                }
+                while (digitalRead(collumPins[c]) == LOW);
+                delay(100);
+                break;
             }
-            pinMode(rowPins[r], INPUT);
         }
-
-        return userChoice;
+        pinMode(rowPins[r], INPUT);
     }
 
-    bool validChar(char c) {
-        if (c != '\0') {
-            Serial.println("validing char");
-            return true;
-        }
+    return userChoice;
+}
 
-        return false;
+bool validChar(char c) {
+    if (c != '\0') {
+        Serial.println("validing char");
+        return true;
     }
+
+    return false;
+}
